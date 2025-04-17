b538d59129504e2fa59abf82c46fa50b92078dbf
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-04-17 08:48:11 +0200

    Ajout du point d'entrée principal pour l'API NestJS

---
+++ apps/api/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.enableCors(); // Enable CORS for development
+  
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+bootstrap();
